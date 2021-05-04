a17154a7e32f71dd6a12c7b9accadba6a9e88532
aemiliaurban <72620408+aemiliaurban@users.noreply.github.com>
2021-05-04 13:43:47 +0200

    Update plot.py
    Changed the code to work with the pandas library

--- plot.py
+++ plot.py
@@ -1,24 +1,31 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
 
-wt = open("wt.regiongs.coverage", "r").readlines()
-tumor = open("tumor.regiongs.coverage", "r").readlines()
+wt = pd.read_table("wt.regiongs.coverage", sep="\t", names=["chr", "pos", "depth"])
+tumor = pd.read_table("tumor.regiongs.coverage", sep="\t", names=["chr", "pos", "depth"])
+i, j = 1, 1 
 
+positions = []
 logs = []
-pos = []
-   
-for el in wt:
-    for l in tumor:
-        ell = el.split()
-        ll = l.split()
-        if ell[1] == ll[1] and el[2] != 0:
-            logs.append(np.log2(int(ll[2])/int(ell[2])))
-            pos.append(ell[1])
-            break
+
+while wt.iloc[i, 0] or tumor.iloc[j, 0]:
+    if wt.iloc[i, 2] == 0 or tumor.iloc[j, 2] == 0:
+        i += 1
+        j += 1
+    elif wt.iloc[i, 1] < tumor.iloc[j, 1]:
+        i += 1
+    elif wt.iloc[i, 1] > tumor.iloc[j, 1]:
+        j += 1
+    else:
+        positions.append(wt.iloc[i, 1])
+        logs.append(np.log2(tumor.iloc[j, 2]/wt.iloc[i, 2]))
+        i += 1
+        j +=1
         
-plt.pyplot.scatter(logs, pos)
-plt.title("Solution")
+plt.scatter(positions, logs)
+plt.title("Read depth scatter plot")
 plt.xlabel("Logarithmic values of depth coverage")
 plt.ylabel(("Position in genome"))
-plt.show()
+plt.savefig("coverage_plot")
             
